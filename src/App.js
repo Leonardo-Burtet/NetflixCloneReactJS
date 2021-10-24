e613b5e57a0215e81a9e86c38ce39a434895a719
Leonardo Burtet <leonardoburtet@outlook.com>
2021-10-24 17:34:45 -0300

    add style App.css &MovieRow.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmds';
 import MovieRow from './components/MovieRow';
+import './App.css';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
